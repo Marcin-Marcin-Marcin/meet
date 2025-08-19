84c585929d6709c1684c135fb55342b842c3ad34
MarcinBiesiada <mtbiesiada@gmail.com>
2025-08-19 20:09:29 +0200

    Atatus

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// --- Atatus (RUM) init ---
+import * as atatus from 'atatus-spa';
+atatus.config('b1c6a6afddf04ea4b1b3a1fe30e35054').install();
+// --- end Atatus init ---
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
