472a0f2bea192618c9c69e65303431173156f669
MarcinBiesiada <mtbiesiada@gmail.com>
2025-08-23 23:35:48 +0200

    Service Worker and Manifest.json

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,12 @@ import * as atatus from 'atatus-spa';
 atatus.config('b1c6a6afddf04ea4b1b3a1fe30e35054').install();
 // --- end Atatus init ---
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
+
+serviceWorkerRegistration.register();
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: false,
+      srcDir: 'src',
+      filename: 'service-worker.js',
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /\/.*\.png$/,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'images',
+              expiration: { maxEntries: 50 }
+            }
+          }
+        ]
+      }
+    })
+  ],
 })
